2a4f4af6c2b49b63f23197eb2291366f718a19b9
genbaku <nazarenko.s2006@gmail.com>
2024-07-15 11:23:30 +0200

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className="text-3xl font-bold mb-4">Home page</h1>
+            <p className="text-lg mb-6">This is my first project using NextJS. Click the button below to view it</p>
+            <Link href="/dashboard">
+                <p className="p-7 px-8 py-2 text-black text-xl rounded bg-gradient-to-br from-purple-200 via-lime-100 to-purple-200">View Project</p>
+            </Link>
+        </div>
     </main>
   );
 }
